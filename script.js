533c7f07ec95d700e9bb26b4703a839e86900355
williademi <125095331+williademi@users.noreply.github.com>
2024-04-24 15:48:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,14 @@ function updateValues() {
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   const income = amounts
     .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1
+  ).toFixed(2);
+
+  balanceEl.innerHTML = `$${total}`;
+  moneyPlusEl.innerHTML = `+$${income}`;
+  moneyMinusEl.innerHTML = `-$${expense}`;
+}
+
